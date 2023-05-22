40dc7593676c6df5fb43edc223addcf4ee10dbdc
Filip Mårtensson <fenwikk@protonmail.com>
2023-05-22 10:18:34 +0200

    Update db.php

--- api/db.php
+++ api/db.php
@@ -5,7 +5,7 @@ $user = getenv("DB_USER");
 $password = getenv("DB_PWD");
 
 $connection = mysqli_init();
-$connection->ssl_set(NULL, NULL, "../ca.pem", NULL, NULL);
+$connection->ssl_set(NULL, NULL, __DIR__ . "/../ca.pem", NULL, NULL);
 $connection->real_connect($host, $user, $password, "wysiwyg");
 
 if ($connection->connect_errno) {
